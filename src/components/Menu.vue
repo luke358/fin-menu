a005ec9302e2232568877a345feed7fc85978929
zxcbb <qq1494135711@gmail.com>
2021-02-23 13:14:58 +0800

    init

---
+++ src/components/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-tree">
+    <!-- 循环全部数据 -->
+    <label v-for="list in dataList" :key="list.id">
+      <!-- 判断是否有孩子 -->
+      <el-submenu :index="list.id" v-if="list.child">
+        <template slot="title">
+          <i class="el-icon-location" v-if="list.level"></i>
+          <span>{{list.name}}</span>
+        </template>
+        <label>
+          <!-- 递归： 调用自己 把自己的孩子传入 -->
+          <child-menu :dataList="list.child"></child-menu>
+        </label>
+      </el-submenu>
+      <!-- 没有孩子则不需要使用 el-submenu -->
+      <el-menu-item v-else :index="list.index">
+        <span slot="title">{{list.name}}</span>
+      </el-menu-item>
+    </label>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'childMenu',
+  props: ['dataList']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-menu>
+   <child-menu :dataList="listData"></child-menu>
+ </el-menu>
 </template>
-
+ 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import childMenu from '../components/Menu.vue'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  components:{
+    childMenu
+  },
+  data(){
+    return {
+      listData :[
+        {
+          name: '钠管设备',
+          id: "1",
+          level: 1,
+          child: [
+            {
+              name: '系统默认组',
+              id: "2",
+              flag: true
+            },
+            {
+              name: '设备一组',
+              id: "3",
+              child: [
+                {
+                  name: '一组A小组',
+                  id: "4",
+                  child: [
+                    {
+                      name: '一组A小组的成员1',
+                      id: "5"
+                    },
+                    {
+                      name: '一组A小组的成员2',
+                      id: "6",
+                      child: [
+                        {
+                          name: '一组A小组的成员何总',
+                          id: "7"
+                        },
+                        {
+                          name: '一组A小组的成员曾',
+                          id: "8"
+                        },
+                        {
+                          name: '一组A小组的成员飘总',
+                          id: "9"
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  name: '一组B小组',
+                  id: "10"
+                }
+              ]
+            },
+            {
+              name: '设备二组',
+              id: "11"
+            }
+          ]
+        }
+      ]
+
+    }
   }
 }
 </script>
